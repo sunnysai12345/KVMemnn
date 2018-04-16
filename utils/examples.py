86e7be87ef37b2c3c93b4d2983ee7f4bf0145fcd
Kondalarao Vonteru <sunnysai12345@gmail.com>
2018-04-16 13:34:36 +0530

    Updates

--- utils/examples.py
+++ utils/examples.py
@@ -12,21 +12,54 @@ from keras.models import Model
 from keras.callbacks import ModelCheckpoint
 from reader import Data,Vocabulary
 from model.memnn import memnn
-
+from math import log
+from numpy import array
+from numpy import argmax
 outdf = {'input': [], 'output': []}
 EXAMPLES = ["find starbucks <eos>", "What will the weather in Fresno be in the next 48 hours <eos>",
             "give me directions to the closest grocery store <eos>", "What is the address? <eos>",
             "Remind me to take pills", "tomorrow in inglewood will it be windy?"]
 
+# beam search
+def beam_search_decoder(data, k):
+    sequences = [[list(), 1.0]]
+    # walk over each step in sequence
+    for row in data:
+        all_candidates = list()
+        # expand each current candidate
+        for i in range(len(sequences)):
+            seq, score = sequences[i]
+            for j in range(len(row)):
+                #print(row[j])
+                if row[j]<=0:
+                    row[j]=0.000000000000000000001
+                candidate = [seq + [j], score * -log(row[j])]
+                all_candidates.append(candidate)
+        # order all candidates by score
+        ordered = sorted(all_candidates, key=lambda tup:tup[1])
+        # select k best
+        sequences = ordered[:k]
+    return sequences
 def run_example(model, kbs,vocabulary, text):
     print(text)
     encoded = vocabulary.string_to_int(text)
     #print("encoded is", encoded)
     prediction = model.predict([np.array([encoded]), kbs])
-    prediction = np.argmax(prediction[0], axis=-1)
+    pred = np.argmax(prediction[0], axis=-1)
+    print(pred.shape)
+    prediction=prediction.reshape((20,1954))
+    result=beam_search_decoder(prediction,5)
+    data=[]
+    for seq in result:
+        #print(seq)
+        #print(np.array(seq[0]).shape)
+        #print(' '.join(vocabulary.int_to_string(np.array(seq[0]))))
+        data.append(' '.join(vocabulary.int_to_string(np.array(seq[0]))))
+    #print("shape of prediction is",type(prediction), prediction.shape)
+
     #print(prediction, type(prediction), prediction.shape)
-    print(prediction.shape, vocabulary.int_to_string(prediction))
-    return vocabulary.int_to_string(prediction)
+    #print(prediction.shape, vocabulary.int_to_string(prediction))
+    return data
 
 
 def run_examples(model, kbs, vocabulary, examples=EXAMPLES):
@@ -35,7 +68,7 @@ def run_examples(model, kbs, vocabulary, examples=EXAMPLES):
     for example in examples:
         print('~~~~~')
         input.append(example)
-        predicted.append(' '.join(run_example(model, kbs, vocabulary, example)))
+        predicted.append(run_example(model, kbs, vocabulary, example))
         outdf['input'].append(example)
         outdf['output'].append(predicted[-1])
     return predicted
@@ -59,7 +92,7 @@ if __name__ == "__main__":
                   embedding_learnable=True,
                   encoder_units=200,
                   decoder_units=200)
-    weights_file = "../weights/model_weights_nkbb.hdf5"
+    weights_file = "../model_weights_nkbbl.hdf5"
     model.load_weights(weights_file, by_name=True)
 
     kbfile = "../data/normalised_kbtuples.csv"
@@ -70,12 +103,13 @@ if __name__ == "__main__":
     kbs = np.repeat(kbs[np.newaxis, :, :], 1, axis=0)
     data = run_examples(model, kbs,vocab, inputs)
     df=pd.DataFrame(columns=["inputs","outputs","prediction"])
-    d = {'outputs':[],'inputs':[],'prediction': []}
+    d = {'outputs':[],'inputs':[],'u1': [],'u2':[],'u3':[],'u4':[],'u5':[]}
     for i, o, p in zip(inputs, outputs, data):
         d["outputs"].append(str(o))
         d["inputs"].append(str(i))
-        d["prediction"].append(str(p))
+        for i,preds in enumerate(p):
+            d["u"+str(i+1)].append(str(preds))
     df = pd.DataFrame(d)
-    df.to_csv("output_nkbb.csv")
+    df.to_csv("output_kb.csv")
     # print(outputs)
 
